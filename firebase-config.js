a1f311575ccb06716c000e9cc7489af6ff29a64b
jorayevsherali040-glitch <jorayevsherali040@gmail.com>
2026-07-23 13:37:04 +0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,18 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBNQ_14GHXIa1s_8_480-e2NAEr7boZlKw",
-  authDomain: "buvayda-ibrat-mebel.firebaseapp.com",
-  projectId: "buvayda-ibrat-mebel",
-  storageBucket: "buvayda-ibrat-mebel.firebasestorage.app",
-  messagingSenderId: "630152385372",
-  appId: "1:630152385372:web:9ba8311493052009532799"
+  apiKey: "AIzaSyAPqQ9a7p2rYRp89EUsaBIBfZhnAMSPdJ4",
+  authDomain: "ibrat-mebelv9.firebaseapp.com",
+  projectId: "ibrat-mebelv9",
+  storageBucket: "ibrat-mebelv9.firebasestorage.app",
+  messagingSenderId: "973246786789",
+  appId: "1:973246786789:web:c1362d2f7886092fc1380c",
+  measurementId: "G-Z4EH42EVR7"
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
